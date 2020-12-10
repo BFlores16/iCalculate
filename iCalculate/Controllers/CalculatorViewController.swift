34c8c66f3a1a7775a483f5e87e5e5b37172cd2ba
Brando <brandoflores16@gmail.com>
2020-12-10 14:16:11 -0700

    Add number and AC, +/-, % functionality

--- iCalculate/Controllers/CalculatorViewController.swift
+++ iCalculate/Controllers/CalculatorViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    private var isFinishedTypingNumber: Bool = true
+    private var userHasPressedDecimal: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +20,45 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var displayLabel: UILabel!
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert label text to a double.")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "AC" {
+                displayLabel.text = "0"
+            }
+            else if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            }
+            else if calcMethod == "%" {
+                displayLabel.text = String(number / 100.00)
+            }
+        }
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            }
+            else {
+                if numValue == "." {
+                    guard let currentDisplayValue = Double(displayLabel.text!) else {
+                        fatalError("Cannot convert display label text to a Double!")
+                    }
+                    let isInt = floor(currentDisplayValue) == currentDisplayValue
+                    if !isInt {
+                        return
+                    }
+                }
+                displayLabel.text?.append(numValue)
+            }
+        }
+        
     }
     
 
