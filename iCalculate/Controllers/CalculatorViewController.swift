ea9d6d92e99bb5adba30a607c4484a90185e6422
Brando <brandoflores16@gmail.com>
2020-12-10 12:49:37 -0700

    Fix IBOutlets

---
+++ iCalculate/Controllers/CalculatorViewController.swift
@@ -0,0 +1,26 @@
+//
+//  CalculatorViewController.swift
+//  iCalculate
+//
+//  Created by Brando Flores on 12/10/20.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+    }
+    
+
+}
